a0c51ba40f2c534b799e1d30e84cc534d600437d
soulchild <soulchildsince2013@gmail.com>
2016-08-25 00:33:25 +0800

    Replaced theme with customized kasper
    Replaced theme with customized kasper

---
+++ Gemfile.rb
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ group :jekyll_plugins do
+   gem "jekyll-watch"
+   gem "jekyll-paginate"
+   gem "gemoji"
+   gem "gemoji-parser"
+   gem "html-pipeline"
+   gem "kramdown"
+   gem "pygments.rb"
+ end
